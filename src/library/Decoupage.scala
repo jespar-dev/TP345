f7df3e3657ac210cc65a684e40b046f5f4d7356b
Jespar <jespar@protonmail.com>
2018-01-30 12:57:20 +0100

    code fonction FiltrageHtml

--- src/library/Decoupage.scala
+++ src/library/Decoupage.scala
@@ -31,7 +31,26 @@ trait FiltrageHtml{
     @param e l'expression 
     @return true si le document satisfait l'expression e
 */
-  def filtreHtml(h:Html,e:Expression):Boolean
+  def filtreHtml(h:Html,e:Expression):Boolean= {
+    e match {
+      case And(e1, e2) => filtreHtml(h, e1) && filtreHtml(h, e2)
+      case Or(e1, e2) => filtreHtml(h, e1) || filtreHtml(h, e2)
+      case Word(w) => findHtml(h, w)
+    }
+  }
+  
+  private def findHtml(h:Html, w:String):Boolean= {
+    h match {
+      case Tag(_, _, children) => {
+        var res = false
+        for(c <- children) {
+          res = res || findHtml(c, w)
+        }
+        res
+      }
+      case Text(t) => t contains w
+    }
+  }
 }
 
 trait ProductionResultat{
